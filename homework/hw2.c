0020cd87bcffe9f405fea9539daf9a4df54de915
Kurivyan <arman.azamatov.2015@mail.ru>
2022-10-02 18:50:07 +0600

    123

---
+++ homework/hw2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int func2(void){
+    printf("\nEnter the char : ");
+    char x; scanf("%c", &x); //Инициализация и запись переменной для работы
+    return x;
+}
+
+
+int main(void) {
+    
+    return 0;
+}
